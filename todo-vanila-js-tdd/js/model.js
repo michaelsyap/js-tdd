8bfa4169a8c37dfd2604e52c2397909e4d0f177e
Michael Yap <michaelyap07@gmail.com>
2020-04-10 09:26:57 +0800

    Implement model tests in jasmine

--- todo-vanila-js-tdd/js/model.js
+++ todo-vanila-js-tdd/js/model.js
@@ -5,17 +5,55 @@
 		this.storage = storage;
 	}
 
-  Model.prototype.createTodo = function(todo) {
-    var self = this;
+  Model.prototype.create = function(todo) {
 
-    try {
-      self.storage.save(todo);
-    } catch(error) {
-      return new Error();
-    }
+    return new Promise((function(resolve, reject){
 
-    return todo;
+      this.storage.save(todo)
+        .then(function onSaveSuccess(data) {
+          resolve(data);
+        })
+
+    }).bind(this))
+
   };
+
+  Model.prototype.read = function(todo) {
+
+    return new Promise((function(resolve, reject) {
+
+      this.storage.findAll()
+        .then(function onReadSuccess(data) {
+          resolve(data)
+        });
+
+    }).bind(this))
+
+  };
+
+
+  Model.prototype.update = function(todo) {
+    return new Promise((function(resolve, reject){
+
+      this.storage.save(todo)
+        .then(function onUpdateSuccess(data) {
+          resolve(data);
+        })
+
+    }).bind(this))
+  }
+
+
+  Model.prototype.delete = function(todo) {
+    return new Promise((function(resolve, reject){
+
+      this.storage.remove(todo)
+        .then(function onDeleteSuccess(data) {
+          resolve(data)
+        })
+
+    }).bind(this))
+  }
 
 
 	// Export to window
